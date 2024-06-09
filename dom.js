ed349e57d861a0f221aeabd28d1bb7ff8643004a
Wahyuatmaja <atmaja007@gmail.com>
2024-06-09 15:49:13 +0700

    Menambahkan Array

---
+++ dom.js
@@ -0,0 +1,67 @@
+document.title ='Belajar Dom'
+
+// //Cara mengisi element ke dalam body html
+// const body = document.body
+
+// const h1 = document.createElement('h1') // Deklarasi variabel membuat elements baru
+// h1.textContent = '<marquee>Ini Joko</marquee>' // isi konten dari elements h1
+ 
+// const namaSaya = document.createElement('p')
+// namaSaya.innerHTML= '<marquee>Nama Saya Adalah Wahyu</marquee>'
+
+// const namaKamu = document.createElement('b')
+// namaKamu.innerText = '<marquee>Nama Kamu adalah Joko</marquee>'
+
+// body.append(namaSaya)
+// body.append(namaKamu)
+// body.append(h1) //Menampilkan isi h1 ke dalam body html
+
+//Selector
+const btn1 = document.getElementById('btn1')
+const btn2 = document.querySelector('.btn2')
+const body = document.body
+
+const defaultText = 'Klik Saya 1'
+btn1.textContent = defaultText
+
+const newText = document.createElement('p')
+newText.textContent = 'Hallo Joko! Apakabar ?'
+
+btn1.style.border ='none'
+btn1.style.padding = '8px'
+btn1.style.borderRadius = '8px'
+btn1.style.background = 'blue'
+btn1.style.color = 'white'
+btn1.style.fontSize = '12px'
+
+// console.log(btn1)
+
+function clickButton() {
+    // console.log('aman')
+    // btn1.style.border = '1px'
+    // btn1.style.background = 'red'
+    // btn1.style.padding = '8px'
+    // btn1.style.borderRadius = '8px'
+    // btn1.style.color = 'white'
+    // btn1.style.fontSize = '12px'
+    // console.log(btn1)
+    document.body.append(newText)
+}   
+
+function ubahText() {
+   btn1.textContent = 'Tekan Button'
+   btn1.style.background = 'green'
+   newText.style.color = 'black'
+   newText.style.fontSize = '24px'
+   newText.style.fontWeight = 'bold'
+   
+}
+
+function oriText() {
+    btn1.textContent =  defaultText
+    btn1.style.background = 'blue'
+    newText.style.color = 'blue'
+    newText.style.fontSize= '48px'
+    newText.style.fontWeight= 'bold'
+    console.log(oriText)
+}
---
+++ main.js
@@ -0,0 +1,69 @@
+// /* 
+// BERSENANDUNG BERSAMA ARRAY
+// **/
+
+// /* Ada 4 cara pemakaian Array **/
+
+// // 1. Array dengan isian
+// const arraySaya = ['Muka', 'Tangan', 'Bibir']
+// console.log(arraySaya) 
+
+// // 2. Array kosong dengan cara mendeklarasikan indexnya
+// const arrayKamu = []
+// arrayKamu[0] = 'Apel'
+// arrayKamu[1] = 'Anggur'
+// arrayKamu[2] = 'Mangga' 
+// console.log(arrayKamu)
+
+// // 3. Array
+// const arrayKita = new Array ()
+// arrayKita[0] = 'Buku'
+// arrayKita[1] = 'Pensil'
+// arrayKita[2] = 'Penghapus'
+// console.log(arrayKita)
+
+// // 4. Array
+// const arrayMereka = new Array('Bola', 'Tikus', 'Kucing')
+// console.log(arrayMereka)
+
+// const arraySaya = ['Alpukat', 'Pear', 'Jambu', 'Semangka', 'Jeruk']
+// const semangka = arraySaya.includes('Semangka')
+
+// // console.log(posisiSemangka)
+// // console.log(semangka)
+
+// if(semangka) {
+//     const posisiSemangka = arraySaya.indexOf('Semangka')
+//     const indexBefore = posisiSemangka - 1
+//     const indexAfter = posisiSemangka + 1
+//     const before = arraySaya[indexBefore]
+//     const after = arraySaya[indexAfter]
+//     const buahAwal = arraySaya.shift()
+//     const buahAkhir = arraySaya.pop()
+
+//     console.log(`Buah Semangka ada diposisi index ke ${posisiSemangka}`)
+//     console.log(`Buah sebelum semangka adalah ${before}`)
+//     console.log(`Buah setelah semangka adalah ${after}`)
+//     console.log(`Buah Pertama adalah ${buahAwal}`)
+//     console.log(`Buah Terakhir adalah ${buahAkhir}`)
+// } else {
+//     console.log('Buah semangka tidak ada dalam array')
+// } 
+
+// Mengubah index kunci dari Array
+// const arraySaya = []
+// arraySaya['A'] = 'Muka'
+// arraySaya['B'] = 'Tangan'
+// arraySaya['C'] = 'Bibir'
+// console.log(arraySaya)
+
+// Array yang tidak boleh mengubah isi original arraynya (Slice() bitCopy) 
+const arraySaya = ['Muka', 'Tangan', 'Bibir', 'Kaki', 'Telinga']
+//const arrayCopy = [...arraySaya] // cara pertama
+//const arrayCopy = arraySaya.slice() // cara kedua
+const arrayCopy = JSON.parse(JSON.stringify(arraySaya)) // Cara Ketiga
+
+arrayCopy[0] = 'Hidung'
+console.log(arraySaya)
+console.log(arrayCopy)
+
